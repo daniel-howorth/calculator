ec07c62c04a7de94109382b7c8972b3a5f773cd4
Daniel Howorth <daniel.howorth@sky.com>
2025-04-25 18:53:46 +0100

    added display styles

--- src/components/Screen/Screen.jsx
+++ src/components/Screen/Screen.jsx
@@ -2,7 +2,12 @@ import React from "react";
 import styles from "./Screen.module.css";
 
 function Screen() {
-  return <div className={styles.screen}></div>;
+  return (
+    <div className={styles.screen}>
+      <span className={styles.entries}>123 * 456</span>
+      <span className={styles.display}>0123456789</span>
+    </div>
+  );
 }
 
 export default Screen;
